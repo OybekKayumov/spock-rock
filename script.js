107293027157eb271749d0edc5f53d71864b57c3
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-03-21 15:05:15 +0500

    Add js

--- script.js
+++ script.js
@@ -26,6 +26,54 @@ const choices = {
   spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
 };
 
+let playerScoreNumber = 0;
+let computerScoreNumber = 0;
+let computerChoice = '';
+
+// random computer choice
+function computerRandomChoice() {
+  const compChoiceNumber = Math.random();
+
+  if (compChoiceNumber < 0.2) {
+    computerChoice = 'rock';
+  } else if (compChoiceNumber <= 0.4) {
+    computerChoice = 'paper'
+  } else if (compChoiceNumber <= 0.6) {
+    computerChoice = 'scissors'
+  } else if (compChoiceNumber <= 0.8) {
+    computerChoice = 'lizard'  
+  } else {
+    computerChoice = 'spock'
+  }
+}
+
+// add 'selected' styling and computerChoice
+function displayComputerChoice() {
+  switch (computerChoice) {
+    case 'rock':
+      computerRock.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Rock';
+      break;
+    case 'paper':
+      computerPaper.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Paper';
+      break;
+    case 'scissors':
+      computerScissors.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Scissors';
+      break;
+    case 'lizard':
+      computerLizard.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Lizard';
+      break;
+    case 'spock':
+      computerSpock.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Spock';
+      break;
+    default:
+      break;
+  }
+
 // reset all selected icons
 function resetSelected() {
   allGameIcons.forEach((icon) => {
@@ -33,11 +81,39 @@ function resetSelected() {
   });
 }
 
+// update score
+function updateScore(playerChoice) {
+  if (playerChoice === computerChoice) {
+    resultText.textContent = "It's a tie."
+  } else {
+    const choice = choices[playerChoice];
+
+    if (choice.defeats.indexOf(computerChoice) > -1) {
+      resultText.textContent = "You Won!";
+      playerScoreNumber++;
+      playerScoreEl.textContent = playerScoreNumber;
+    } else {
+      resultText.textContent = "You Lost!";
+      computerScoreNumber++;
+      computerScoreEl.textContent = computerScoreNumber;
+    }
+  }
+}
+
+// call functions to process turn
+function checkResult(playerChoice) {
+  resetSelected();
+
+  computerRandomChoice();
+  displayComputerChoice();
+
+  updateScore(playerChoice);
+}
 
 // Passing player selection value and styling icons
 function select(playerChoice) {
-  resetSelected();
-  
+  checkResult(playerChoice); 
+
   // add selected styling and update playerChoice
   switch (playerChoice) {
     case 'rock':
